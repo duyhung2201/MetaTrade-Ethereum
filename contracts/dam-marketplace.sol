b85ac0fc081ad6e5767259582e78b5aa0436edc2
tuanvu <tuanvu171@DESKTOP-U5K27HM.localdomain>
2022-09-18 20:21:19 -0400

    upload files

---
+++ contracts/dam-marketplace.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Declare errors
+error notEnoughListingFee();
+
+// smart contract
+contract damMartketplace {
+    // Data type definition
+    struct customerAssetData {
+        bytes32 symmetricKey;
+        bytes20 encryptedFileHash;
+        string ipfsURI;
+    }
+
+    struct ownerAssetData {
+        address assetOwner;
+        uint256 assetPrice;
+        string descriptionUri;
+    }
+
+    // State variables
+    uint256 public immutable listingFee;
+    address contractOnwer;
+    uint256 private assetId = 0;
+    // assetId => customer address => asset data that the customer wants to get
+    mapping(uint256 => mapping(address => customerAssetData))
+        private assetIdToCustomerData;
+    mapping(address => uint256) private userFund;
+    mapping(address => uint256) private userFundWithdrawable;
+    mapping(uint256 => ownerAssetData) public assetIdToOnwerData;
+
+    // Event declaration
+    event listedAsset(uint256 indexed assetId);
+
+    // Modifier declaration
+
+    // Constructor
+    constructor(uint256 listingFeeInitialized) {
+        listingFee = listingFeeInitialized;
+        contractOnwer = msg.sender;
+    }
+
+    // Function definition
+    function listAsset(string memory descriptionUri, uint256 assetPrice)
+        public
+        payable
+    {
+        if (msg.value < listingFee) {
+            revert notEnoughListingFee();
+        }
+        assetIdToOnwerData[assetId] = ownerAssetData(
+            msg.sender,
+            assetPrice,
+            descriptionUri
+        );
+        assetId += 1;
+        userFundWithdrawable[contractOnwer] += msg.value;
+        emit listedAsset(assetId);
+    }
+
+    function cancelListing(uint256 assetIdToCancel) public {}
+}
